0a53a8d2627d59d8c6a0a2bb07720404bdbf19ed
BopaxDev <marionicdematteis@gmail.com>
2023-12-24 16:05:16 +0100

    Add Test

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,4 +30,7 @@ using Test
     @test Δ("EuropeanPut", 50, 52, 0.05, 0.1, 0.5) == -0.5658254795944931
     @test Δ("EuropeanPut", 30, 40, 0.1, 0.5, 0.65) == -0.6371494797327102
 
+    @test Δ("Sample1", 30, 40, 0.1, 0.5, 0.65) == Nothing()
+    @test Δ("Sample2", 30, 40, 0.1, 0.5, 0.65) == Nothing()
+
 end
