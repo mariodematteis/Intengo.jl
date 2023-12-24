1eab3423db12893888385252951427fda0b634d8
BopaxDev <marionicdematteis@gmail.com>
2023-12-24 12:45:36 +0100

    Add LogNormal Prices Simulation

---
+++ src/Simulation/Simulation.jl
@@ -0,0 +1,6 @@
+using Distributions
+
+function StockPricesSimulation(𝖲₀::Union{Float64,Int}, μ::Union{Float64,Int}, σ::Union{Float64,Int}, P::Int, S::Int)
+    dW = rand(Normal(0, sqrt(1 / S)), P, S)
+    return transpose(𝖲₀ .* exp.(cumsum(((μ / S) - ((σ / sqrt(S))^2 / 2)) .+ (σ * dW), dims=2)))
+end
